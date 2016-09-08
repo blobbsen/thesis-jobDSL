20468067a0a89801be587c0f23114a500671ff2d
Andre Boddenberg <andre.boddenberg@here.com>
2016-09-08 22:05:10 +0200

    Adding closure and method which holds a whole step on node

--- pipeline/common/common.groovy
+++ pipeline/common/common.groovy
@@ -1,5 +1,15 @@
+{ ->
+ echo 'loading common.groovy' to make steps available within this pipeline'
+}
+
 def hello(whom) {
   sh 'echo "hello ${whom}"'
+}
+
+def wholeMasterStep() {
+node('master') {
+  sh 'injected via common.groovy'
+}
 }
 
 return this;
